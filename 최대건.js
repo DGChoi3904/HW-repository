3d834635c7f1b4ee43d800a5bb90130895f64da7
dgchoi3904 <dgchoi3904@gmail.com>
2023-05-02 22:41:51 +0900

    작성한 클래스를 외부로 옮기고 해당 클래스를 import하여 사용하는 방식으로 변환함.

--- 최대건.js
+++ 최대건.js
@@ -3,8 +3,13 @@
 // const divide = require('./utility-module/divide.js');
 // const multiply = require('./utility-module/multiply.js');
 
+// 위의 사칙연산 스크립트를 하나로 모아서 만들었다고 가정하고, 그렇게 만든 클래스를 require를 통해 import함.
+const FAO = require('./utility-module/FAO.js');
+
 const a = 10;
 const b = 20;
+
+
 
 // console.log(add(a,b));
 // console.log(minus(a,b));
@@ -15,47 +20,14 @@ const b = 20;
 // Q.1 아무래도 이렇게 하나하나 가져오는 것은 가독성면에서나 사용성면에서 좋아보이지 않습니다.
 // Q.1 이것을 하나의 Class 형태의 모듈로 만들어 관리할 수 있으까요?
 
-//만일 하나로 통합하여 관리한다면, 아래와 같이 클래스를 만들면 된다.
+//만일 하나로 통합하여 관리한다면, 클래스를 만들면 된다.
 //하나의 클래스에 각각의 모듈을 helper function으로 추가하면 된다.
 //여기서 FAO는 사칙연산, Four Fundamental Arthimetic Operation의 줄임말이다.
 
-class FAO{
-  constructor(){
-  }
-  get a(){
-    return this._a;
-  }
-  set a(a){
-    this._a = a;
-  }
-  get b(){
-    return this._b;
-  }
-  set b(b){
-    this._b = b;
-  }
-  add(a,b){
-    return a+b;
-  }
-  minus(a,b){
-    return a-b;
-  }
-  divide(a,b){
-    return a/b;
-  }
-  multiply(a,b){
-    return a*b;
-  }
-  reminder(a,b){
-    return a%b;
-  }
-
-}
-const fourAO = new FAO();
-console.log(fourAO.add(a,b));
-console.log(fourAO.minus(a,b));
-console.log(fourAO.divide(a,b));
-console.log(fourAO.multiply(a,b));
+console.log(FAO.add(a,b));
+console.log(FAO.minus(a,b));
+console.log(FAO.divide(a,b));
+console.log(FAO.multiply(a,b));
 
 
 // Q.2 Class에서 작성될 메서드는 모두 '정수'로 된 두 개의 인자를 받아서 결과를 반환하게끔 제한하려면,
@@ -68,9 +40,9 @@ console.log(fourAO.multiply(a,b));
 // 만일 데이터 a를 1로 나눌시 나머지가 0일경우 정수임을 판단할 수 있다. 음수이든 양수이든 1로 나누어서 나머지가 0이 아니면 정수가 아니기 떄문이다.
 
 // 10이라는 정수를 가진 a를 1로 나눌경우, 나머지는 0이 되기에 true가 된다.
-console.log(fourAO.reminder(a,1) === 0);
+console.log(FAO.reminder(a,1) === 0);
 // 11.1이라는 소숫점을 가진 수를 1로 나눌경우, 나머지는 0보다 크기에 false가 된다.
-console.log(fourAO.reminder(11.1,1) == 0);
+console.log(FAO.reminder(11.1,1) == 0);
 
 // helper function을 사용할경우, 해당 클래스가 스크립트 안에 import되거나 선언되고 할당되는 한, 각각 따로 import할 필요 없이 
 // object의 내장 메서드 호출방식처럼 불러서 사용할 수 있다.
@@ -79,7 +51,7 @@ console.log(fourAO.reminder(11.1,1) == 0);
 // 이를 리터럴로 사용하여 구현할 수 있다.
 
 // 해당 helper function만 변수를 선언하고 담아 사용하기.
-const adder = fourAO.add;
+const adder = FAO.add;
 //adder는 fourAO.add() 함수와 동일한 작업을 한다. 결과는 a+b;
 console.log(adder(a,b));
 
